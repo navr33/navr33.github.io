a718a67b7db378811914d012202017a827e46d42
Nestor <nessval33@gmail.com>
2024-09-14 09:50:20 -0300

    EO4 data viewer. Item data

---
+++ EO4/database.js
@@ -0,0 +1,590 @@
+var data = {
+"Landsknecht":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "After you attack, other allies deal more damage and have more accuracy against your targets until the end of turn.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Damage Dealt↑": ["+20%", "+25%", "+30%"],
+                "Accuracy↑": ["+10%", "+15%", "+20%"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Nightseeker":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Drastically increases damage dealt against enemies with ailments.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Damage Dealt↑": ["+100%", "+140%", "+180%"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Fortress":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "If you're in the front line, recover TP each time you take or dodge a hit.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "TP Gain": ["3", "4", "5"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Sniper":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Allows Bow skills to perform Critical Hits, and increases the Critical damage bonus.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Crit Bonus↑": ["+20%", "+35%", "+50%"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Medic":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Increases the effect of healing skills used by any party member.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Healing↑": ["+15%", "+20%", "+25%"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Runemaster":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Increases damage dealt and decreases damage taken of elemental or Almighty type for party members in your row.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Damage Dealt↑": ["+10%", "+13%", "+15%"],
+                "Damage Taken↓": ["-5%", "-8%", "-10%"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Dancer":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Increases the duration of Dance buffs.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Duration↑": ["+1", "+2", "+3"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Arcanist":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Recover TP when a Circle wears off or is dispelled.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "TP Gain": ["5", "8", "10"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Bushi":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Recover TP when you perform a normal attack.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "TP Gain": ["8", "10", "12"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Imperial":{
+    "Class Proficiency":{
+        "Proficiency / Expertise / Enlightment":{
+            "Usage": "Passive (Unique)",
+            "Description": "Decreases the duration of the Overheat state.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "Duration↓": ["-1", "-2", "-3"],
+            }
+        },
+    },
+    "Novice (Lv 1)":{
+
+    },
+    "Veteran (Lv 20)":{
+
+    },
+    "Master (Lv 40)":{
+
+    },
+},
+"Battle Items":{
+    "Healing":{
+        "Medica / Medica II / Medica III / Medica IV":{
+            "Usage": "20 / 500 / 2.000 / 8.000 en",
+            "Unlock": "N/A / Luminous Moss / Aloe / Ambrosia",
+            "Description": "Restores HP to one ally.",
+            "Starter": "0",
+            "Max Level": "4",
+            "Data": {
+                "HP Gain": ["50", "100", "200", "All"],
+            }
+        },
+        "Nectar / Nectar II":{
+            "Usage": "200 / 7.000 en",
+            "Unlock": "Small Flower / Raptor Claw",
+            "Description": "Revives one ally.",
+            "Starter": "0",
+            "Max Level": "2",
+            "Data": {
+                "HP Gain": ["20", "All"],
+            }
+        },
+        "Amrita / Amrita II / Amrita III":{
+            "Usage": "1.000 / 8.000 / 20.000 en",
+            "Unlock": "Crane Ginseng / Waterlily Leaf / Eerie Scales",
+            "Description": "Restores TP to one ally.",
+            "Starter": "0",
+            "Max Level": "3",
+            "Data": {
+                "TP Gain": ["30", "60", "100"],
+            }
+        },
+        "Hamao / Hamao II":{
+            "Usage": "2.000 / 10.000 en",
+            "Unlock": "Scarlet Sage / Deathly Bud",
+            "Description": "Restores HP and TP to one ally.",
+            "Starter": "0",
+            "Max Level": "2",
+            "Data": {
+                "HP Gain": ["100", "200"],
+                "TP Gain": ["25", "50"],
+            }
+        },
+        "Madora / Madora II":{
+            "Usage": "1.500 / 15.000 en",
+            "Unlock": "Flame Fruit / Grub Mold",
+            "Description": "Restores HP to an ally line.",
+            "Starter": "0",
+            "Max Level": "2",
+            "Data": {
+                "HP Gain": ["150", "300"],
+            }
+        },
+    },
+    "Status":{
+        "Theriaca A / Theriaca B":{
+            "Usage": "100 / 90 en",
+            "Unlock": "Osmunda Frond / Forest Aloe",
+            "Description": "Respectively removes binds or ailments from one ally.",
+            "Starter": "0",
+            "Max Level": "0",
+            "Levels": ["1"],
+            "Data": ""
+        },
+        "Metopon":{
+            "Usage": "1.000 en",
+            "Unlock": "Myrciaria Fruit, Sealed Cap",
+            "Description": "Removes all buffs on one enemy.",
+            "Starter": "0",
+            "Max Level": "0",
+            "Data": ""
+        },
+        "Unihorn":{
+            "Usage": "500 en",
+            "Unlock": "Valerian",
+            "Description": "Removes all debuffs on an ally line.",
+            "Starter": "0",
+            "Max Level": "0",
+            "Data": ""
+        },
+    },
+    "Power Up":{
+        "Bravant":{
+            "Usage": "300 en",
+            "Unlock": "Stunned Tongue",
+            "Description": "Places a buff on one ally for 3 turns, which increases physical damage dealt.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Damage Dealt↑": ["+30%"],
+            }
+        },
+        "Stonard":{
+            "Usage": "150 en",
+            "Unlock": "Moss Strawberry",
+            "Description": "Places a buff on one ally for 3 turns, which decreases physical damage taken.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Damage Taken↓": ["-30%"],
+            }
+        },
+        "Fire/Ice/Volt Mist":{
+            "Usage": "150 en",
+            "Unlock": "Monks Cress",
+            "Description": "Places a buff on all allies for 3 turns, which increases resistance to Fire/Ice/Volt.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Resistance↑": ["+50%"],
+            }
+        },
+        "Cut/Bash/Stab Mist":{
+            "Usage": "300 en",
+            "Unlock": "Life Honey",
+            "Description": "Places a buff on all allies for 3 turns, which increases resistance to Cut/Bash/Stab.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Resistance↑": ["+50%"],
+            }
+        },
+        "Blaze/Freeze/Shock Oil":{
+            "Usage": "75 en",
+            "Unlock": "Dragon's Blood",
+            "Description": "Places a buff on one ally for 3 turns, which imbues their weapon with Fire/Ice/Volt damage.",
+            "Starter": "0",
+            "Max Level": "0",
+            "Data": ""
+        },
+    },
+    "Attack (TEC)":{
+        "Fire Jar":{
+            "Usage": "100 en",
+            "Unlock": "Mint",
+            "Description": "Deals ranged Fire damage to one enemy, with splash effect.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["50%"],
+                "Action Speed": ["-9"],
+            }
+        },
+        "Blast Jar":{
+            "Usage": "500 en",
+            "Unlock": "Hyacinth Petal",
+            "Description": "Deals ranged Fire damage to all enemies.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["50%"],
+                "Action Speed": ["-9"],
+            }
+        },
+        "Ice Jar":{
+            "Usage": "100 en",
+            "Unlock": "Mint",
+            "Description": "Deals ranged Ice damage to one enemy, with pierce effect.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["50%"],
+                "Action Speed": ["-9"],
+            }
+        },
+        "Frost Jar":{
+            "Usage": "500 en",
+            "Unlock": "Hyacinth Petal",
+            "Description": "Deals 2~4 hits of ranged Ice damage to random enemies.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["40%"],
+                "Action Speed": ["-9"],
+            }
+        },
+        "Volt Jar":{
+            "Usage": "100 en",
+            "Unlock": "Mint",
+            "Description": "Deals ranged Volt damage to an enemy line.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["40%"],
+                "Action Speed": ["-9"],
+            }
+        },
+        "Thunderbolt Jar":{
+            "Usage": "500 en",
+            "Unlock": "Hyacinth Petal",
+            "Description": "Deals ranged Volt damage to one enemy.",
+            "Starter": "0",
+            "Max Level": "1",
+            "Data": {
+                "Attack Power": ["90%"],
+                "Action Speed": ["-9"],
+            }
+        },
+    },
+    "Infliction (LUC)":{
+        "Poison/Nerve/Blind/Sleep/Curse/Addle Gas":{
+            "Usage": "100 / 200 / 150 / 300 / 800 / 800 en",
+            "Unlock": "Shea Fruit / Glove Grass / Gritty Scales / White Apple / Cursed Durian / Evil Copal",
+            "Description": "Respectively attempts to inflict Poison, Paralysis, Blind, Sleep, Curse or Panic on all enemies.",
+            "Starter": "0",
+            "Max Level": "6",
+            "Levels": ["Poi", "Par", "Bli", "Sle", "Cur", "Pan"],
+            "Data": {
+                "Infliction Rate": ["40%", "40%", "40%", "40%", "40%", "30%"],
+                "Poison Power": ["50", "N/A", "N/A", "N/A", "N/A", "N/A"],
+                "Action Speed": ["-6", "-6", "-6", "-6", "-6", "-6"],
+            }
+        },
+    },
+    "Rewards":{
+        "Formaldehyde":{
+            "Usage": "500 en",
+            "Unlock": "Horrific Breath",
+            "Description": "Until the end of turn, enemies killed will drop all their materials.",
+            "Starter": "0",
+            "Max Level": "0",
+            "Data": ""
+        },
+    }
+},
+}
+
+//End of database
+
+var descriptions = {
+    "Landsknecht":"Frontline warriors that encourage allies to follow up after their attacks.",
+    "Nightseeker":"Assassins that hide to poison enemies then close in to kill them.",
+    "Fortress":   "Sturdy knights that put themselves on the line to protect the party.",
+    "Sniper":     "Archers that aim at weak spots to incapacitate or deal heavy damage.",
+    "Medic":      "Healers that effectively treat the party's injuries and afflictions.",
+    "Runemaster": "Offensive casters that control the elements to hit the enemy's weakness.",
+    "Dancer":     "Performers who use buffs to strengthen allies and attack alongside them.",
+    "Arcanist":   "Support casters that summon magic circles to restrain the enemy.",
+    "Bushi":      "Fierce attackers that enter a bloody rage to trade health for power.",
+    "Imperial":   "Knights whose mechanical blades deliver devastating but exhausting attacks.",
+
+    "Burst Skills":"Powerful skills available through the Burst system.",
+    "Battle Items":"Consumable items meant to aid you in battle.",
+}
+
+function toggle_data(table_id){
+    document.getElementById(table_id).classList.toggle("show_table");
+}
+
+function populate(class_name){
+    var skill_list = document.getElementById("skill_list")
+    while (skill_list.hasChildNodes()){
+        skill_list.removeChild(skill_list.firstChild)
+    }
+    var current_class = data[class_name]
+    name_banner = document.createElement("h2")
+    name_banner.style.margin = "20px"
+    name_banner.innerText = class_name
+    skill_list.appendChild(name_banner)
+
+    class_decription = document.createElement("p")
+    class_decription.style.marginLeft = "10px"
+    class_decription.innerText = descriptions[class_name]
+    skill_list.appendChild(class_decription)
+    for (const category in current_class){
+        category_name = document.createElement("h3")
+        category_name.innerText = category
+        skill_list.appendChild(category_name)
+        for (const skill in current_class[category]){
+            var current_skill = current_class[category][skill]
+            skill_table = document.createElement("table")
+            skill_table.classList.add("skill_entry")
+            if (current_skill["Starter"] == "2"){
+                skill_table.style.marginTop = "15px"
+            }
+            skill_list.appendChild(skill_table)
+            
+            first_row = document.createElement("tr")
+            skill_table.appendChild(first_row)
+
+            skill_name = document.createElement("th")
+            first_row.appendChild(skill_name)
+            skill_name.classList.add("skill_name")
+            skill_name.innerText = skill
+            skill_name.colSpan = 2
+
+            second_row = document.createElement("tr")
+            skill_table.appendChild(second_row)
+
+            usage = document.createElement("td")
+            second_row.appendChild(usage)
+            usage.innerText = current_skill["Usage"]
+
+            if (current_skill.hasOwnProperty("Unlock")){
+                usage.width = "30%"
+                unlock = document.createElement("td")
+                second_row.appendChild(unlock)
+                unlock.innerText = "Unlock: "+current_skill["Unlock"]
+            }
+            
+            third_row = document.createElement("tr")
+            skill_table.appendChild(third_row)
+
+            data_block = document.createElement("td")
+            third_row.appendChild(data_block)
+            data_block.classList.add("hide_table")
+            data_block.style.textAlign = "left"
+            data_block.colSpan = 2
+            data_block.id = skill
+            first_row.addEventListener("click", function(){
+                toggle_data(skill)
+            })
+
+            description = document.createElement("p")
+            data_block.appendChild(description)
+            description.innerHTML = current_skill["Description"]
+
+            levels_table = document.createElement("table")
+            data_block.appendChild(levels_table)
+            levels_table.classList.add("levels_table")
+            levels_row = document.createElement("tr")
+            levels_table.appendChild(levels_row)
+            if (current_skill["Data"] !== ""){
+                start_level = document.createElement("th")
+                start_level.innerText = "Level"
+                levels_row.appendChild(start_level)
+            }
+            for (let i = 1; i <= current_skill["Max Level"]; i++){
+                level = document.createElement("th")
+                levels_row.appendChild(level)
+                if (current_skill.hasOwnProperty("Levels")){
+                    level.innerText = current_skill["Levels"][i-1]
+                }
+                else{
+                    level.innerText = i
+                }
+                
+            }
+            for (const effect in current_skill["Data"]){
+                effect_row = document.createElement("tr")
+                levels_table.appendChild(effect_row)
+
+                effect_name = document.createElement("th")
+                effect_name.innerText = effect
+                effect_row.appendChild(effect_name)
+                current_effect = current_skill["Data"][effect]
+                latest = current_effect
+                var latest
+                for (const value in current_effect){
+                    if (current_effect[value] == latest.innerText) {
+                        latest.colSpan = latest.colSpan+1
+                        continue
+                    }
+                    effect_value = document.createElement("td")
+                    effect_value.innerText = current_effect[value]
+                    effect_row.appendChild(effect_value)
+                    latest = effect_value
+                }
+            }
+        }
+    }
+}
+
+populate("Landsknecht")
+
+
+
